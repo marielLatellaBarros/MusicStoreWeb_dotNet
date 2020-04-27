0e1ff8801a926d9aacb187566fe8bd0f407ed92d
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-27 16:19:59 +0200

    Add tests skeleton for HomeController

---
+++ MusicStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace MusicStore.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Index_ReturnsContentContainingControllerNameAndActionName()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void About_ReturnsContentContainingControllerNameAndActionName()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
