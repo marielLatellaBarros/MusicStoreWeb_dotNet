5c946d771d1052989413b4d0c40b766da54637ea
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-28 10:48:09 +0200

    Change return type of Index method to be of ContentResult to be able to access the Content property

--- MusicStore.Web/Controllers/HomeController.cs
+++ MusicStore.Web/Controllers/HomeController.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Diagnostics;
 using System.Linq;
 
 namespace MusicStore.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ContentResult Index()
         {
             var message = ControllerContext.RouteData.Values.Values.Aggregate("", (current, value) => current + value);
 
