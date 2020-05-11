4127d81736407973005e1e48cd23a2441c5d241f
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-05-11 14:38:54 +0200

    *** PART 2*** Add data layer and domain classes album and genre
    - .Net Standard class library

---
+++ MusicStoreCore.Data/DomainClasses/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicStoreCore.Data.DomainClasses
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int GenreId { get; set; }
+
+    }
+}
